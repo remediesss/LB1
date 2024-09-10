9798503181436ed52db98fa344104ded3427fdd7
remediesss <167404465+remediesss@users.noreply.github.com>
2024-09-10 18:04:49 +0300

    Update Task5.cpp

--- Task5.cpp
+++ Task5.cpp
@@ -1,41 +1,118 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <cstdint>
+#include <locale.h>
+#include <stdlib.h>
 
-typedef struct {
-    char lastName[50];
-    char firstName[50];
+#define MAX_NAME_LENGTH 50
+#define MAX_FACULTY_LENGTH 100
+
+typedef struct Student {
+    char lastName[MAX_NAME_LENGTH];
+    char firstName[MAX_NAME_LENGTH];
+    int course;
+    char faculty[MAX_FACULTY_LENGTH];
+    struct Student* next;
 } Student;
 
-int findStudent(Student students[], int size, const char* lastName, const char* firstName) {
-    for (size_t i = 0; i < size; ++i) {
-        if (strcmp(students[i].lastName, lastName) == 0 && strcmp(students[i].firstName, firstName) == 0) {
-            return i; 
-        }
+Student* createStudent(const char* lastName, const char* firstName, int course, const char* faculty) {
+    Student* student = (Student*)malloc(sizeof(Student));
+    strcpy(student->lastName, lastName);
+    strcpy(student->firstName, firstName);
+    student->course = course;
+    strcpy(student->faculty, faculty);
+    student->next = NULL;
+    return student;
+}
+
+void printStudent(Student* student) {
+    printf("Фамилия: %s\n", student->lastName);
+    printf("Имя: %s\n", student->firstName);
+    printf("Курс: %d\n", student->course);
+    printf("Факультет: %s\n", student->faculty);
+}
+
+void addStudent(Student** head, Student* student) {
+    if (*head == NULL) {
+        *head = student;
     }
-    return -1; 
+    else {
+        Student* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = student;
+    }
+}
+
+Student* findStudent(Student* head, const char* lastName, const char* firstName) {
+    Student* current = head;
+    while (current != NULL) {
+        if (strcmp(current->lastName, lastName) == 0 && strcmp(current->firstName, firstName) == 0) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+void freeList(Student* head) {
+    Student* current = head;
+    while (current != NULL) {
+        Student* next = current->next;
+        free(current);
+        current = next;
+    }
 }
 
 int main() {
-    Student students[] = {
-        {"Ivanov", "Ivan"},
-        {"Petrov", "Petr"},
-        {"Sidorov", "Sidr"}
-    };
 
-    int size = sizeof(students) / sizeof(students[0]);
+    setlocale(LC_ALL, "Russian");
 
-    const char* searchFirstName = "Petr";
-    const char* searchLastName = "Petrov";
+    Student* head = NULL;
 
-    int index = findStudent(students, size, searchLastName, searchFirstName);
+    while (1) {
+        char lastName[MAX_NAME_LENGTH];
+        char firstName[MAX_NAME_LENGTH];
+        int course;
+        char faculty[MAX_FACULTY_LENGTH];
 
-    if (index != -1) {
-        printf("The student was found: %s %s\n", students[index].lastName, students[index].firstName);
+        printf("Введите фамилию студента (или '~' для завершения ввода): ");
+        scanf("%s", lastName);
+        if (strcmp(lastName, "~") == 0) {
+            break;
+        }
+
+        printf("Введите имя студента: ");
+        scanf("%s", firstName);
+        printf("Введите курс студента: ");
+        scanf("%d", &course);
+        printf("Введите факультет студента: ");
+        scanf("%s", faculty);
+
+        Student* student = createStudent(lastName, firstName, course, faculty);
+        addStudent(&head, student);
+    }
+
+    char searchLastName[MAX_NAME_LENGTH];
+    char searchFirstName[MAX_NAME_LENGTH];
+
+    printf("Введите фамилию студента для поиска: ");
+    scanf("%s", searchLastName);
+    printf("Введите имя студента для поиска: ");
+    scanf("%s", searchFirstName);
+
+    Student* foundStudent = findStudent(head, searchLastName, searchFirstName);
+
+    if (foundStudent != NULL) {
+        printf("Студент найден:\n");
+        printStudent(foundStudent);
     }
     else {
-        printf("The student was not found.\n");
+        printf("Студент не найден.\n");
     }
+
+    freeList(head);
 
     return 0;
 }
